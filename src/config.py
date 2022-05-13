f1d96cd6c3b9a33da50dfc74a5053b290c52982d
nassimb0u <in_boulechfar@esi.dz>
2022-05-14 00:42:49 +0100

    refactor and update requirements.txt

--- src/config.py
+++ src/config.py
@@ -3,11 +3,11 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(32)
+    DEFAULT_ITEM_PER_PAGE = 5
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    DEFAULT_ITEM_PER_PAGE = 5
     MONGODB_SETTINGS = {
         "host": os.environ.get("DB_HOST", "127.0.0.1"),
         "port": 27017,
