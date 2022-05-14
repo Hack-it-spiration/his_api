cd4dd54b805d800e6d0ef213a39731a7d3b41433
nassimb0u <in_boulechfar@esi.dz>
2022-05-14 08:00:30 +0100

    allow cors

--- src/app.py
+++ src/app.py
@@ -1,19 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 
 from src.config import config
 from src.features.common.response_handlers import error_handlers
+from src.features.ml_model.blueprint import model as model_blueprint
 from src.features.toll_roads.checkpoint.blueprint import (
     checkpoints as checkpoints_blueprint,
 )
 from src.features.toll_roads.segment.blueprint import segments as segments_blueprint
 
-from src.features.ml_model.blueprint import model as model_blueprint
-
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    CORS(app, ressources={r"*": {"origins": "*"}})
     db = MongoEngine()
     db.init_app(app)
     app.register_blueprint(checkpoints_blueprint)
